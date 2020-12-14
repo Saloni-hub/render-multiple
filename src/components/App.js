9588cca635ad2d46587e2341a2d1d53e7e8ba3be
Saloni Pandey <70425604+Saloni-hub@users.noreply.github.com>
2020-12-14 19:04:43 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+
+const App = () => {
+  return (
+    <>
+    <h1 data-ns-test="project-name">E-Commerce App </h1>
+    <p data-ns-test="project-description">Ever since e-commerce penetrated the
+     mainstream industry, e-commerce apps (like Amazon, Flipkart, Nykaa, etc.)
+    have become omnipresent. These apps are now an integral part of our lives.
+    Thus, if you build a functional e-commerce app, it will show that you have real-world skills to keep up with the latest trends in the industry. Needless to say, 
+    you will become an appealing candidate before potential employers.</p>
+    </>
+  )
+}
+
+
+export default App;
